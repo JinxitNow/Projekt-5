cd7c6c872ab4462febe2bb086b19383fb9930dba
NicolineZacho <ninezacho@gmail.com>
2025-05-28 10:06:48 +0200

    Forbedring

--- slide.js
+++ slide.js
@@ -1,28 +1,26 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".carousel-slide");
-const dots = document.querySelectorAll(".dot");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.display = i === index ? "flex" : "none";
-    dots[i].classList.toggle("active", i === index);
-  });
-  currentSlide = index;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function nextSlide() {
-  let newIndex = (currentSlide + 1) % slides.length;
-  showSlide(newIndex);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function prevSlide() {
-  let newIndex = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(newIndex);
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
-
-function goToSlide(index) {
-  showSlide(index);
-}
-
-// Init
-showSlide(currentSlide);
